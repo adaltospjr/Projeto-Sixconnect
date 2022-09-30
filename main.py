7cb75688e08af4c41902d8007505f760d2d5b7bb
adaltospjr <adaltospjr@gamil.com>
2022-09-30 14:52:50 -0300

    Testes Recaptcha.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template, request, request
+from flask import render_template, request, request
 from flask_sqlalchemy import SQLAlchemy
 from twitter import busca
 from envio import novos_clientes, clientes_antigo
 from conexao import Clientes, Funcionarios, Equipamentos, db, app
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
+
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdoUiMiAAAAAC2Tp_8caBVKI_PzUE45CELwRis3'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdoUiMiAAAAAMdyUEFyNno-3Xt0k1-bB-LRS4JV'
+app.config['TESTING'] = True
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired('A username is required!'), Length(min=5, max=10, message='Must be between 5 and 10 characters.')])
+    password = PasswordField('password', validators=[InputRequired('Password is required!'), AnyOf(values=['password', 'secret'])])
+    recaptcha = RecaptchaField()
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+    return render_template('form.html', form=form)
 
 @app.route('/')
 def main():
