e26b1ca1488021a9883ca80f09954dae968a752b
Adalto Linhares <adaltospjr@gmail.com>
2022-11-09 19:41:59 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from flask import render_template, request, request
-from flask_sqlalchemy import SQLAlchemy
 from twitter import busca
-from envio import novos_clientes, clientes_antigo
+from envio import novos_clientes, clientes_antigo, pagamento_servico
 from conexao import Clientes, Funcionarios, Equipamentos, db, app
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length, AnyOf
 from pagamento import pagamento
 
 
@@ -13,28 +9,43 @@ from pagamento import pagamento
 def main():
     return render_template('index.html')
 
+
 @app.route('/ficha')
 def get_ficha():
     return render_template('ficha.html')
+
 
 @app.route('/ficha',  methods=["POST"])
 def cadastrar_ficha():
     nome = str(request.form.get("nome"))
     servico = str(request.form.get("servico"))
     email = str(request.form.get("email"))
-    
-    pay = pagamento(nome=nome, servico=servico, email=email)
+    valor = float(request.form.get("valor"))
+    validador = nome.replace(" ", "")
 
-    return render_template('consultarficha.html', pay=pay)
+    if not valor >= 0:
+        message = "Por favor, digite um valor positivo"
+        return render_template("consultarficha.html", message=message)
 
-#@app.route('/consultarficha')
-#def consulta_ficha():
-#    clientes = Clientes.query.all()
-#    return render_template('consultaclientes.html', clientes=clientes)
+    elif not validador.isalpha():
+        message = "Por favor, digite somente letras no campo nome."
+        return render_template("consultarficha.html", message=message)
+
+    pay = pagamento(valor=valor, servico=servico, email=email,  nome=nome)
+
+    id_compra = pay['id']
+
+    pay = pay["point_of_interaction"]["transaction_data"]["ticket_url"]
+
+    pagamento_servico(email, pay)
+
+    return render_template('consultarficha.html', pay=pay, id_compra=id_compra)
+
 
 @app.route('/cadastrarfuncionarios')
 def get_funcionarios():
     return render_template('cadastrarfuncionarios.html')
+
 
 @app.route('/cadastrarfuncionarios', methods=["POST"])
 def cadastrar_funcionarios():
@@ -46,7 +57,7 @@ def cadastrar_funcionarios():
         message = "Por favor, digite somente letras no campo nome"
         return render_template("cadastrarfuncionarios.html", message=message)
     else:
-        # class instance 
+        # class instance
         funcionario = Funcionarios(name=name, email=email, senha=senha)
         # crio o registro no banco
         funcionario.create_funcionario()
@@ -54,9 +65,11 @@ def cadastrar_funcionarios():
         funcionarios = funcionario.query.all()
         return render_template('consultafuncionarios.html', funcionarios=funcionarios)
 
+
 @app.route('/cadastrarclientes')
 def get_clientes():
     return render_template('cadastrarclientes.html')
+
 
 @app.route('/cadastrarclientes', methods=["POST"])
 def cadastrar_clientes():
@@ -73,9 +86,11 @@ def cadastrar_clientes():
         novos_clientes(email)
         return render_template('consultaclientes.html', clientes=clientes)
 
+
 @app.route('/cadastrarequipamentos')
 def get_equipamentos():
     return render_template('cadastrarequipamentos.html')
+
 
 @app.route('/cadastrarequipamentos', methods=["POST"])
 def cadastrar_equipamentos():
@@ -94,29 +109,34 @@ def cadastrar_equipamentos():
         equipamentos = equipamento.query.all()
         return render_template('consultaequipamentos.html', equipamentos=equipamentos)
 
+
 @app.route('/consultaequipamentos')
 def consulta_equipamentos():
     equipamentos = Equipamentos.query.all()
     return render_template('consultaequipamentos.html', equipamentos=equipamentos)
+
 
 @app.route('/consultafuncionarios')
 def consulta_funcionarios():
     funcionarios = Funcionarios.query.all()
     return render_template('consultafuncionarios.html', funcionarios=funcionarios)
 
+
 @app.route('/consultaclientes')
 def consulta_clientes():
     clientes = Clientes.query.all()
     return render_template('consultaclientes.html', clientes=clientes)
 
+
 @app.route('/deleteclientes')
 def get_deletar_clientes():
     return render_template('deleteclientes.html')
 
+
 @app.route('/deleteclientes', methods=["POST"])
 def deletar_clientes():
     id = int(request.form.get("id"))
-    if  id <= 0:
+    if id <= 0:
         message = "Por favor, digite somente números positivos no campo id."
         return render_template("deleteclientes.html", message=message)
     elif id in Clientes.consultar_id_clientes():
@@ -129,14 +149,16 @@ def deletar_clientes():
         message = "Id não encontrado, digite um id válido."
         return render_template("deleteclientes.html", message=message)
 
+
 @app.route('/deletefuncionarios')
 def get_deletar_funcionarios():
     return render_template('deletefuncionarios.html')
 
+
 @app.route('/deletefuncionarios', methods=["POST"])
 def deletar_funcionarios():
     id = int(request.form.get("id"))
-    if  id <= 0:
+    if id <= 0:
         message = "Por favor, digite somente números positivos no campo id."
         return render_template("deletefuncionarios.html", message=message)
     elif id in Funcionarios.consultar_id_funcionario():
@@ -147,14 +169,16 @@ def deletar_funcionarios():
         message = "Id não encontrado, digite um id válido."
         return render_template("deletefuncionarios.html", message=message)
 
+
 @app.route('/deleteequipamento')
 def get_deletar_equipamentos():
     return render_template('deleteequipamentos.html')
 
+
 @app.route('/deleteequipamento', methods=["POST"])
 def deletar_equipamentos():
     id = int(request.form.get("id"))
-    if  not id >= 0:
+    if not id >= 0:
         message = "Por favor, digite somente números positivos no campo id."
         return render_template("deleteequipamentos.html", message=message)
     elif id in Equipamentos.consultar_id_equipamentos():
@@ -165,9 +189,11 @@ def deletar_equipamentos():
         message = "Id não encontrado, digite um id válido."
         return render_template("deleteequipamentos.html", message=message)
 
+
 @app.route('/atualizarclientes')
 def get_atualizar_cliente():
     return render_template('atualizarclientes.html')
+
 
 @app.route('/atualizarclientes', methods=["POST"])
 def atualizar_cliente():
@@ -189,15 +215,17 @@ def atualizar_cliente():
         message = "Id não encontrado, digite um id válido."
         return render_template("atualizarclientes.html", message=message)
 
+
 @app.route('/atualizarequipamentos')
 def get_atualizar_equipamento():
     return render_template('atualizarequipamentos.html')
+
 
 @app.route('/atualizarequipamentos', methods=["POST"])
 def atualizar_equipamento():
     id = int(request.form.get("id"))
     nome = request.form.get("nome")
-    codigo = request.form.get("codigo") 
+    codigo = request.form.get("codigo")
     qtde = int(request.form.get("qtde"))
     if not id >= 0:
         message = "Por favor, digite somente números positivos no campo id."
@@ -212,9 +240,11 @@ def atualizar_equipamento():
     else:
         return "Id não encontrado, digite um id válido"
 
+
 @app.route('/atualizarfuncionarios')
 def get_atualizar_funcionario():
     return render_template('atualizarfuncionarios.html')
+
 
 @app.route('/atualizarfuncionarios', methods=["POST"])
 def atualizar_funcionario():
@@ -230,7 +260,7 @@ def atualizar_funcionario():
         return render_template("atualizarfuncionarios.html", message=message)
     elif nome.isalpha() == False:
         message = "Por favor, digite um id com número positivo."
-        return render_template("atualizarfuncionarios.html", message=message)    
+        return render_template("atualizarfuncionarios.html", message=message)
     elif id in Funcionarios.consultar_id_funcionario():
         Funcionarios.atualizar_funcionarios(id, nome, email)
         funcionarios = Funcionarios.query.all()
@@ -239,11 +269,14 @@ def atualizar_funcionario():
         message = "Id não encontrado, digite um id válido"
         return render_template("atualizarfuncionarios.html", message=message)
 
+
 @app.route('/twitter')
 def twitter():
     retorno_busca = busca()
     return render_template('twitter.html', retorno_busca=retorno_busca)
 
-if __name__ =="__main__":
-	db.create_all()
-	app.run(debug=True)
+
+if (__name__ == "__main__"):
+    with app.app_context() as ctx:
+        db.create_all()
+    app.run()
